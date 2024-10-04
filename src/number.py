b22acaaa19757b0d199d9eb7d1562a4017456e91
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2024-10-04 09:07:21 +0100

    :bug: (ruff) fix

--- src/number.py
+++ src/number.py
@@ -1,5 +1,9 @@
 from typing import Iterable
 
+from src.util import print_hello
+
 
 def sum_even_numbers(numbers: Iterable[int]) -> int:
+    print_hello()
+
     return sum(num for num in numbers if num % 2 == 0)
